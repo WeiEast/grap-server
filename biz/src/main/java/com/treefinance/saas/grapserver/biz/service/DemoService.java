4d39d11ba3dab36aeda8245efea92e7796687b8d
haojiahong <haojiahong@treefinance.com.cn>
2017-10-17 15:42:21 +0800

    demo

---
+++ biz/src/main/java/com/treefinance/saas/grapserver/biz/service/DemoService.java
@@ -0,0 +1,16 @@
+package com.treefinance.saas.grapserver.biz.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * H5界面演示系统业务处理类
+ * Created by haojiahong on 2017/10/17.
+ */
+@Service
+public class DemoService {
+
+    public Object getFundData(String params) {
+
+        return null;
+    }
+}
---
+++ web/src/main/java/com/treefinance/saas/grapserver/web/controller/DemoController.java
@@ -0,0 +1,41 @@
+package com.treefinance.saas.grapserver.web.controller;
+
+import com.treefinance.saas.grapserver.biz.service.DemoService;
+import com.treefinance.saas.knife.result.Results;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by haojiahong on 2017/10/17.
+ */
+@RestController
+@RequestMapping(value = {"/grap/h5/demo"})
+public class DemoController {
+    private static final Logger logger = LoggerFactory.getLogger(DemoController.class);
+
+    @Autowired
+    private DemoService demoService;
+
+    @RequestMapping(value = "/fund/data", method = {RequestMethod.POST})
+    public Object createTask(@RequestParam("appid") String appid,
+                             @RequestParam("params") String params,
+                             HttpServletRequest request) {
+        if (StringUtils.isBlank(appid) || StringUtils.isBlank(params)) {
+            throw new IllegalArgumentException("Parameter is incorrect.");
+        }
+        Object result = demoService.getFundData(params);
+        return Results.newSuccessResult(result);
+
+
+    }
+
+
+}
