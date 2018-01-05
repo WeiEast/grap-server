f99a552c7684223469eb2a9d42c8fcabaa798a50
haojiahong <haojiahong@treefinance.com.cn>
2018-01-05 17:13:07 +0800

    h5邮箱账单

--- biz/src/main/java/com/treefinance/saas/grapserver/biz/service/EmailLoginSimulationService.java
+++ biz/src/main/java/com/treefinance/saas/grapserver/biz/service/EmailLoginSimulationService.java
@@ -1,9 +1,11 @@
 package com.treefinance.saas.grapserver.biz.service;
 
 import com.alibaba.fastjson.JSON;
+import com.datatrees.rawdatacentral.api.CommonPluginApi;
 import com.datatrees.rawdatacentral.api.mail.qq.MailServiceApiForQQ;
-import com.datatrees.rawdatacentral.domain.mail.MailParam;
+import com.datatrees.rawdatacentral.domain.plugin.CommonPluginParam;
 import com.datatrees.rawdatacentral.domain.result.HttpResult;
+import com.datatrees.rawdatacentral.domain.result.ProcessResult;
 import com.treefinance.saas.grapserver.common.exception.CrawlerBizException;
 import com.treefinance.saas.knife.result.SimpleResult;
 import org.apache.commons.lang3.StringUtils;
@@ -11,8 +13,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Map;
 
 /**
  * 邮箱账单模拟登陆
@@ -25,24 +25,26 @@ public class EmailLoginSimulationService {
 
     @Autowired
     private MailServiceApiForQQ mailServiceApiForQQ;
+    @Autowired
+    private CommonPluginApi commonPluginApi;
 
     /**
      * 登陆
      *
-     * @param mailParam
+     * @param param
      * @return
      */
-    public Object login(MailParam mailParam) {
-        HttpResult<Map<String, String>> result;
+    public Object login(CommonPluginParam param) {
+        HttpResult<Object> result;
         try {
-            result = mailServiceApiForQQ.login(mailParam);
+            result = mailServiceApiForQQ.login(param);
         } catch (Exception e) {
-            logger.error("邮箱账单:调用爬数邮箱账单登陆异常,mailParam={}", JSON.toJSONString(mailParam), e);
+            logger.error("邮箱账单:调用爬数邮箱账单登陆异常,param={}", JSON.toJSONString(param), e);
             throw e;
         }
         if (!result.getStatus()) {
-            logger.info("邮箱账单:调用爬数邮箱账单登陆失败,mailParam={},result={}",
-                    JSON.toJSONString(mailParam), JSON.toJSONString(result));
+            logger.info("邮箱账单:调用爬数邮箱账单登陆失败,param={},result={}",
+                    JSON.toJSONString(param), JSON.toJSONString(result));
             if (StringUtils.isNotBlank(result.getMessage())) {
                 throw new CrawlerBizException(result.getMessage());
             } else {
@@ -53,24 +55,20 @@ public class EmailLoginSimulationService {
     }
 
     /**
-     * 登陆状态(前端轮询)
+     * 轮询处理状态(通用接口)
      *
-     * @param mailParam
+     * @param processId
+     * @param taskId
      * @return
      */
-    public Object loginStatus(MailParam mailParam) {
-        HttpResult<Map<String, String>> result;
+    public Object processStatus(Long processId, Long taskId) {
+        ProcessResult result;
         try {
-            result = mailServiceApiForQQ.queryLoginStatus(mailParam);
+            result = commonPluginApi.queryProcessResult(processId);
         } catch (Exception e) {
-            logger.error("邮箱账单:调用爬数邮箱账单轮询登陆状态异常,emailParam={}", JSON.toJSONString(mailParam), e);
+            logger.error("邮箱账单:调用爬数邮箱账单轮询处理状态异常,processId={},taskId={}", processId, taskId, e);
             throw e;
         }
-        if (!result.getStatus()) {
-            logger.info("邮箱账单:调用爬数邮箱账单轮询登陆状态失败,mailParam={},result={}",
-                    JSON.toJSONString(mailParam), JSON.toJSONString(result));
-            throw new CrawlerBizException(result.getMessage());
-        }
-        return SimpleResult.successResult(result.getData());
+        return SimpleResult.successResult(result);
     }
 }
--- web/src/main/java/com/treefinance/saas/grapserver/web/controller/EmailH5Controller.java
+++ web/src/main/java/com/treefinance/saas/grapserver/web/controller/EmailH5Controller.java
@@ -1,7 +1,7 @@
 package com.treefinance.saas.grapserver.web.controller;
 
 import com.alibaba.fastjson.JSON;
-import com.datatrees.rawdatacentral.domain.mail.MailParam;
+import com.datatrees.rawdatacentral.domain.plugin.CommonPluginParam;
 import com.treefinance.saas.grapserver.biz.service.EmailLoginSimulationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,37 +24,37 @@ public class EmailH5Controller {
     /**
      * 登陆
      *
-     * @param mailParam
+     * @param param
      * @return
      */
     @RequestMapping(value = "/login/submit", method = {RequestMethod.POST})
-    public Object login(MailParam mailParam) {
-        logger.info("邮箱账单:登陆,传入参数,emailParam={}", JSON.toJSONString(mailParam));
-        if (mailParam == null || mailParam.getTaskId() == null) {
-            logger.error("邮箱账单:登陆,参数缺失,taskId必传,emailParam={}", JSON.toJSONString(mailParam));
+    public Object login(CommonPluginParam param) {
+        logger.info("邮箱账单:登陆,传入参数,param={}", JSON.toJSONString(param));
+        if (param == null || param.getTaskId() == null) {
+            logger.error("邮箱账单:登陆,参数缺失,taskId必传,param={}", JSON.toJSONString(param));
             throw new IllegalArgumentException("邮箱账单:登陆,参数缺失,taskId必传");
         }
-        Object result = emailLoginSimulationService.login(mailParam);
-        logger.info("邮箱账单:登陆,返回结果,emailParam={},result={}", JSON.toJSONString(mailParam), JSON.toJSONString(result));
+        Object result = emailLoginSimulationService.login(param);
+        logger.info("邮箱账单:登陆,返回结果,param={},result={}", JSON.toJSONString(param), JSON.toJSONString(result));
         return result;
     }
 
-
     /**
-     * 登陆状态(前端轮询)
+     * 轮询处理状态接口(通用接口)
      *
-     * @param mailParam
+     * @param processId
+     * @param taskId
      * @return
      */
-    @RequestMapping(value = "/login/status", method = {RequestMethod.POST})
-    public Object loginStatus(MailParam mailParam) {
-        logger.info("邮箱账单:轮询登陆状态,传入参数,emailParam={}", JSON.toJSONString(mailParam));
-        if (mailParam == null || mailParam.getTaskId() == null) {
-            logger.error("邮箱账单:轮询登陆状态,参数缺失,taskId必传,emailParam={}", JSON.toJSONString(mailParam));
+    @RequestMapping(value = "/process/status", method = {RequestMethod.POST})
+    public Object processStatus(Long processId, Long taskId) {
+        logger.info("邮箱账单:轮询处理状态,传入参数,processId={},taskId={}", processId, taskId);
+        if (taskId == null) {
+            logger.error("邮箱账单:轮询处理状态,参数缺失,taskId必传");
             throw new IllegalArgumentException("邮箱账单:轮询登陆状态,参数缺失,taskId必传");
         }
-        Object result = emailLoginSimulationService.loginStatus(mailParam);
-        logger.info("邮箱账单:轮询登陆状态,返回结果,emailParam={},result={}", JSON.toJSONString(mailParam), JSON.toJSONString(result));
+        Object result = emailLoginSimulationService.processStatus(processId, taskId);
+        logger.info("邮箱账单:轮询登陆状态,返回结果,processId={},taskId={},result={}", processId, taskId, JSON.toJSONString(result));
         return result;
     }
 
