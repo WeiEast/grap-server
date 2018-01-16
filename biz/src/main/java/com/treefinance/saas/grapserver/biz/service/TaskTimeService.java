96956af4a0abd579244909d1708f18d6ad5e0938
haojiahong <haojiahong@treefinance.com.cn>
2018-01-16 19:30:04 +0800

    taskTimeService bean

--- biz/src/main/java/com/treefinance/saas/grapserver/biz/service/TaskTimeService.java
+++ biz/src/main/java/com/treefinance/saas/grapserver/biz/service/TaskTimeService.java
@@ -24,6 +24,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
@@ -37,6 +38,7 @@ import java.util.stream.Collectors;
  * 任务时间Service
  * Created by yh-treefinance on 2017/8/3.
  */
+@Service
 public class TaskTimeService {
     /**
      * logger
