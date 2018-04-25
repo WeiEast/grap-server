56af06c1ebe1164bc6f778b1bc58e29e98bca2ec
haojiahong <haojiahong@treefinance.com.cn>
2018-04-25 14:19:08 +0800

    回调前一个环节无数据不统计

--- biz/src/main/java/com/treefinance/saas/grapserver/biz/service/monitor/EmailMonitorService.java
+++ biz/src/main/java/com/treefinance/saas/grapserver/biz/service/monitor/EmailMonitorService.java
@@ -3,10 +3,8 @@ package com.treefinance.saas.grapserver.biz.service.monitor;
 import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
-import com.treefinance.saas.assistant.model.TaskEcommeceMonitorMessage;
 import com.treefinance.saas.assistant.model.TaskEmailMonitorMessage;
 import com.treefinance.saas.assistant.model.TaskStep;
-import com.treefinance.saas.assistant.plugin.TaskEcommerceMonitorPlugin;
 import com.treefinance.saas.assistant.plugin.TaskEmailMonitorPlugin;
 import com.treefinance.saas.grapserver.biz.service.TaskAttributeService;
 import com.treefinance.saas.grapserver.biz.service.TaskBuryPointLogService;
@@ -16,7 +14,6 @@ import com.treefinance.saas.grapserver.common.enums.ETaskStep;
 import com.treefinance.saas.grapserver.common.model.dto.TaskDTO;
 import com.treefinance.saas.grapserver.common.utils.DataConverterUtils;
 import com.treefinance.saas.grapserver.dao.entity.TaskAttribute;
-import com.treefinance.saas.grapserver.dao.entity.TaskBuryPointLog;
 import com.treefinance.saas.grapserver.dao.entity.TaskLog;
 import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
@@ -66,39 +63,45 @@ public class EmailMonitorService {
 
         // 2.获取任务步骤
         List<TaskStep> taskSteps = Lists.newArrayList();
+        Map<Integer, TaskStep> taskStepMap = Maps.newHashMap();
+
         List<TaskLog> taskLogs = taskLogService.queryTaskLog(taskId, ETaskStep.TASK_CREATE.getText(),
-                ETaskStep.LOGIN_SUCCESS.getText(), ETaskStep.CRAWL_SUCCESS.getText(), ETaskStep.DATA_SAVE_SUCCESS.getText(),
-                ETaskStep.CALLBACK_SUCCESS.getText());
+                ETaskStep.LOGIN_SUCCESS.getText(), ETaskStep.LOGIN_FAIL.getText(), ETaskStep.CRAWL_SUCCESS.getText(),
+                ETaskStep.DATA_SAVE_SUCCESS.getText(), ETaskStep.CALLBACK_SUCCESS.getText());
         List<String> taskLogMsgs = taskLogs.stream().map(TaskLog::getMsg).collect(Collectors.toList());
         // 任务创建
         if (taskLogMsgs.contains(ETaskStep.TASK_CREATE.getText())) {
-            taskSteps.add(new TaskStep(1, EProcessStep.CREATE.getCode(), EProcessStep.CREATE.getName()));
+            taskStepMap.put(1, new TaskStep(1, EProcessStep.CREATE.getCode(), EProcessStep.CREATE.getName()));
         }
-        // 一键登录或者登录成功
+        // 确认登录
+        if (taskLogMsgs.contains(ETaskStep.LOGIN_SUCCESS.getText()) || taskLogMsgs.contains(ETaskStep.LOGIN_FAIL.getText())) {
+            taskStepMap.put(2, new TaskStep(2, EProcessStep.CONFIRM_LOGIN.getCode(), EProcessStep.CONFIRM_LOGIN.getName()));
+        }
+        //登录成功
         if (taskLogMsgs.contains(ETaskStep.LOGIN_SUCCESS.getText())) {
-            taskSteps.add(new TaskStep(2, EProcessStep.CONFIRM_LOGIN.getCode(), EProcessStep.CONFIRM_LOGIN.getName()));
-            taskSteps.add(new TaskStep(3, EProcessStep.LOGIN.getCode(), EProcessStep.LOGIN.getName()));
-        } else {
-            // H5版本基于埋点数据，判断是否一键登录
-            List<TaskBuryPointLog> taskBuryPointLogs = taskBuryPointLogService.queryTaskBuryPointLogByCode(taskId,
-                    "200804", "200807");
-            if (CollectionUtils.isNotEmpty(taskBuryPointLogs)) {
-                taskSteps.add(new TaskStep(2, EProcessStep.CONFIRM_LOGIN.getCode(), EProcessStep.CONFIRM_LOGIN.getName()));
-            }
+            taskStepMap.put(3, new TaskStep(3, EProcessStep.LOGIN.getCode(), EProcessStep.LOGIN.getName()));
+
         }
         // 爬取成功
         if (taskLogMsgs.contains(ETaskStep.CRAWL_SUCCESS.getText())) {
-            taskSteps.add(new TaskStep(4, EProcessStep.CRAWL.getCode(), EProcessStep.CRAWL.getName()));
+            taskStepMap.put(4, new TaskStep(4, EProcessStep.CRAWL.getCode(), EProcessStep.CRAWL.getName()));
         }
         // 数据保存成功
         if (taskLogMsgs.contains(ETaskStep.DATA_SAVE_SUCCESS.getText())) {
-            taskSteps.add(new TaskStep(5, EProcessStep.PROCESS.getCode(), EProcessStep.PROCESS.getName()));
+            taskStepMap.put(5, new TaskStep(5, EProcessStep.PROCESS.getCode(), EProcessStep.PROCESS.getName()));
         }
         // 回调成功
         if (taskLogMsgs.contains(ETaskStep.CALLBACK_SUCCESS.getText())) {
-            taskSteps.add(new TaskStep(6, EProcessStep.CALLBACK.getCode(), EProcessStep.CALLBACK.getName()));
+            taskStepMap.put(6, new TaskStep(6, EProcessStep.CALLBACK.getCode(), EProcessStep.CALLBACK.getName()));
         }
 
+        //判断任务步骤是否正确或有遗漏
+        for (int i = 1; i <= 6; i++) {
+            if (!taskStepMap.keySet().contains(i)) {
+                break;
+            }
+            taskSteps.add(taskStepMap.get(i));
+        }
         message.setTaskSteps(taskSteps);
 
         // 4.发送消息
