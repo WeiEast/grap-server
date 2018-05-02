ac7a312aac83ebbac3d963335d48d688b9ad4b9f
haojiahong <haojiahong@treefinance.com.cn>
2018-05-02 11:49:57 +0800

    取消任务 环境问题

--- biz/src/main/java/com/treefinance/saas/grapserver/biz/service/TaskTimeService.java
+++ biz/src/main/java/com/treefinance/saas/grapserver/biz/service/TaskTimeService.java
@@ -119,66 +119,6 @@ public class TaskTimeService {
         return new Date(Long.valueOf(dateTime));
     }
 
-
-    /**
-     * 处理登录后抓取任务超时
-     */
-    @Scheduled(cron = "0 0/1 * * * ?")
-    public void scheduleTask2Timeout() {
-        String key = prefix + "scheduling";
-        if (redisTemplate.opsForValue().setIfAbsent(key, "1")) {
-            try {
-                redisTemplate.expire(key, 1, TimeUnit.MINUTES);
-                Set<String> taskIdSet = redisTemplate.opsForSet().members(taskSetKey);
-                logger.info("scheduleTaskTimeout：running ：lock-key={}, taskid-key={}，taskIds={}", key, taskSetKey, taskIdSet);
-                if (CollectionUtils.isEmpty(taskIdSet)) {
-                    logger.info("scheduleTaskTimeout：taskIds is empty, key={}", taskSetKey);
-                    return;
-                }
-                // 处理超时任务
-                List<Long> taskIds = taskIdSet.stream().map(id -> Long.valueOf(id)).collect(Collectors.toList());
-                for (List<Long> _taskIds : Lists.partition(taskIds, 200)) {
-
-                    TaskCriteria criteria = new TaskCriteria();
-                    criteria.createCriteria().andIdIn(_taskIds);
-                    List<Task> tasks = taskMapper.selectByExample(criteria);
-                    if (CollectionUtils.isEmpty(tasks)) {
-                        continue;
-                    }
-                    // 清除已经完成任务
-                    List<String> completedTaskIds = tasks.stream().filter(task -> !ETaskStatus.RUNNING.getStatus().equals(task.getStatus()))
-                            .map(task -> task.getId().toString()).collect(Collectors.toList());
-                    if (CollectionUtils.isNotEmpty(completedTaskIds)) {
-                        redisTemplate.opsForSet().remove(taskSetKey, completedTaskIds.toArray(new String[]{}));
-                        logger.info("scheduleTaskTimeout : task is completed : completedTaskIds={}", completedTaskIds);
-                    }
-
-                    List<String> timeoutTaskIds = Lists.newArrayList();
-                    // 计算正在运行任务是否有超时
-                    tasks.stream().filter(task -> ETaskStatus.RUNNING.getStatus().equals(task.getStatus()))
-                            .forEach(task -> {
-                                Long taskId = task.getId();
-                                if (handleTaskTimeout(task)) {
-                                    timeoutTaskIds.add(taskId.toString());
-                                }
-                            });
-                    // 清除已经处理的超时任务
-                    if (CollectionUtils.isNotEmpty(timeoutTaskIds)) {
-                        redisTemplate.opsForSet().remove(taskSetKey, timeoutTaskIds.toArray(new String[]{}));
-                        logger.info("scheduleTaskTimeout : task is timeout : timeoutTaskIds={}", timeoutTaskIds);
-                    }
-                }
-
-            } finally {
-                redisTemplate.delete(key);
-            }
-            return;
-        }
-        redisTemplate.expire(key, 30, TimeUnit.SECONDS);
-        logger.info("scheduleTaskTimeout：task is running in other node : key={}", key);
-    }
-
-
     /**
      * 处理登录后抓取任务超时
      */
