7d3a3d3982230f3369edbf1b3458ed56abba9e75
李梁杰 <liliangjie@treefinance.com.cn>
2018-10-19 02:22:46 +0800

    同盾接口结果封装fix

--- biz/src/main/java/com/treefinance/saas/grapserver/biz/service/TongdunService.java
+++ biz/src/main/java/com/treefinance/saas/grapserver/biz/service/TongdunService.java
@@ -1,10 +1,9 @@
 package com.treefinance.saas.grapserver.biz.service;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.alibaba.rocketmq.shade.com.alibaba.fastjson.JSONArray;
 import com.treefinance.saas.grapserver.biz.config.DiamondConfig;
-import com.treefinance.saas.grapserver.biz.service.monitor.MonitorService;
 import com.treefinance.saas.grapserver.common.enums.EBizType;
 import com.treefinance.saas.grapserver.common.enums.ETaskStatus;
 import com.treefinance.saas.grapserver.common.enums.ETongdunData;
@@ -29,74 +28,77 @@ import java.util.*;
 public class TongdunService {
 
     private static final Logger logger = LoggerFactory.getLogger(TongdunService.class);
-    @Autowired private TaskService taskService;
-    @Autowired private TaskLicenseService taskLicenseService;
-    @Autowired private TaskLogService taskLogService;
-    @Autowired private MonitorService monitorService;
-    @Autowired private AppLicenseService appLicenseService;
-    @Autowired private DiamondConfig diamondConfig;
+    @Autowired
+    private TaskService taskService;
+    @Autowired
+    private TaskLicenseService taskLicenseService;
+    @Autowired
+    private TaskLogService taskLogService;
+    @Autowired
+    private AppLicenseService appLicenseService;
+    @Autowired
+    private DiamondConfig diamondConfig;
 
     public Long startCollectTask(String appId, TongdunRequest tongdunRequest) {
         // 使用身份证号当作uniqueId
         taskLicenseService.verifyCreateTask(appId, tongdunRequest.getIdCard(), EBizType.TONGDUN);
-        Long taskId =
-            taskService.createTask(tongdunRequest.getIdCard(), appId, EBizType.TONGDUN.getCode(), null, null, null);
-        return taskId;
+        return taskService.createTask(tongdunRequest.getIdCard(), appId, EBizType.TONGDUN.getCode(), null, null, null);
     }
 
     public Object processCollectTask(Long taskId, String appId, TongdunRequest tongdunRequest) {
         String url = diamondConfig.getTongdunUrlCollect();
         JSONObject data = JSON.parseObject(JSON.toJSONString(tongdunRequest));
-        Map map = new HashMap();
+        Map<String, Object> map = new HashMap<>(1);
         map.put("data", data);
         String httpResult;
         try {
             httpResult = HttpClientUtils.doPost(url, map);
-
         } catch (Exception e) {
             logger.error("调用功夫贷同盾采集任务异常:taskId={},tongdunRequset={}", taskId, tongdunRequest, e);
             taskLogService.insert(taskId, "调用功夫贷同盾采集任务异常", new Date(), "调用功夫贷同盾采集任务异常");
             taskService.updateTaskStatus(taskId, ETaskStatus.FAIL.getStatus());
-            return SimpleResult.failResult("功夫贷同盾采集任务采集失败");
-        }
-        JSONObject result = JSON.parseObject(httpResult);
-        JSONArray saasRuleScoreDTOArrays = JSONArray.parseArray(result.get("saasRuleScoreDTO").toString());
-        Map<String, String> saasRuleScoreDTOMap = new HashMap();
-        for (int i = 0; i < saasRuleScoreDTOArrays.size(); i++) {
-
-            JSONObject saasRuleScoreDTO = JSONObject.parseObject(saasRuleScoreDTOArrays.get(i).toString());
-            saasRuleScoreDTOMap.put(saasRuleScoreDTO.get("ruleName").toString(), saasRuleScoreDTO.get("policyScore").toString());
-
+            return SimpleResult.failResult("Unexpected exception!");
         }
 
-        List<TongdunData> tongdunDataList = new ArrayList<>();
+        JSONObject result = null;
+        try {
+            result = JSON.parseObject(httpResult);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
+        if (result == null) {
+            logger.error("调用功夫贷同盾采集任务返回值中任务日志信息存在问题:taskId={},tongdunRequest={},httpResult={}", taskId, tongdunRequest,
+                httpResult);
+            taskLogService.insert(taskId, "调用功夫贷同盾采集任务返回值中任务日志信息存在问题", new Date(), "调用功夫贷同盾采集任务返回值中任务日志信息存在问题");
+            taskService.updateTaskStatus(taskId, ETaskStatus.FAIL.getStatus());
+            // 错误日志中
+            return SimpleResult.failResult("Unexpected exception!");
+        }
 
-        for (ETongdunData item : ETongdunData.values()) {
+        Map<String, Integer> saasRuleScoreDTOMap = new HashMap<>(8);
+        // 获取规则评分
+        JSONArray scores = result.getJSONArray("saasRuleScoreDTO");
+        for (int i = 0, size = scores.size(); i < size; i++) {
+            JSONObject item = scores.getJSONObject(i);
+            saasRuleScoreDTOMap.put(item.getString("ruleName"), item.getInteger("policyScore"));
+        }
+
+        // 获取详细数值
+        JSONObject summary = result.getJSONObject("saasSummaryDTO");
+        ETongdunData[] values = ETongdunData.values();
+        List<TongdunData> tongdunDataList = new ArrayList<>(values.length);
+        for (ETongdunData item : values) {
             TongdunData tongdunData = new TongdunData();
             tongdunData.setId(item.getName());
-            tongdunData.setValue(item.getText());
-            if (saasRuleScoreDTOMap.get(item.getText()) == null) {
-                tongdunData.setScore(TongdunDataResolver.encode(0));
-            } else {
-
-                tongdunData.setScore(TongdunDataResolver.encode(Integer.valueOf(saasRuleScoreDTOMap.get(item.getText()))));
-            }
+            tongdunData.setValue(TongdunDataResolver.to(summary.getInteger(item.getText())));
+            tongdunData.setScore(TongdunDataResolver.to(saasRuleScoreDTOMap.get(item.getText())));
             tongdunDataList.add(tongdunData);
         }
 
-        if (result != null) {
-
-            AppLicense license = appLicenseService.getAppLicense(appId);
-            taskLogService.insert(taskId, "任务成功", new Date(), "");
-            taskService.updateTaskStatus(taskId, ETaskStatus.SUCCESS.getStatus());
-            return SimpleResult.successEncryptByRSAResult(tongdunDataList, license.getServerPublicKey());
-        } else {
-            logger.error("调用功夫贷同盾采集任务返回值中任务日志信息存在问题:taskId={},tongdunRequest={},httpResult={},result={}", taskId,
-                tongdunRequest, httpResult, JSON.toJSONString(result));
-            taskLogService.insert(taskId, "调用功夫贷同盾采集任务返回值中任务日志信息存在问题", new Date(), "调用功夫贷同盾采集任务返回值中任务日志信息存在问题");
-            taskService.updateTaskStatus(taskId, ETaskStatus.FAIL.getStatus());
-            return SimpleResult.failResult("功夫贷同盾采集任务采集失败");
-        }
+        AppLicense license = appLicenseService.getAppLicense(appId);
+        taskLogService.insert(taskId, "任务成功", new Date(), "");
+        taskService.updateTaskStatus(taskId, ETaskStatus.SUCCESS.getStatus());
+        return SimpleResult.successEncryptByRSAResult(tongdunDataList, license.getServerPublicKey());
     }
 
 }
--- common/src/main/java/com/treefinance/saas/grapserver/common/enums/ETongdunData.java
+++ common/src/main/java/com/treefinance/saas/grapserver/common/enums/ETongdunData.java
@@ -7,22 +7,25 @@ import java.util.Objects;
  * @date:Created in 2018/10/18下午7:12
  */
 public enum ETongdunData {
-    MP_LOAN_12M("MP_LOAN_12M","loanApply12MCntCopy", (byte)1), MP_LOAN_6M("MP_LOAN_6M","loanApply6MCntCopy", (byte)2),
-    MP_LOAN_3M("MP_LOAN_3M","loanApply3MCntCopy", (byte)3), MP_LOAN_1M("MP_LOAN_1M","loanApply1MCntCopy", (byte)4),
-    MD_LOAN_1W("MD_LOAN_1W","loanApply7DCntCopy", (byte)5), MD_IDCARD_1M("MD_IDCARD_1M","identityAssociatedDevice1MCntCopy", (byte)6),
-    MD_IDCARD_1W("MD_IDCARD_1W","deviceAssociatedIdentity7DCntCopy", (byte)7), MM_DEVICE_1W("MM_DEVICE_1W","deviceAssociatedPhone7DCntCopy", (byte)8),
-    MI_DEVICE_1W("MI_DEVICE_1W","identityAssociatedDevice7DCntCopy", (byte)9),
-    MI_MOBILE_3W("MI_MOBILE_3W","phoneAssociatedIdentity3MCntCopy", (byte)10),
-    ME_IDCARD_3M("ME_IDCARD_3M","identityAssociatedMail3MCntCopy", (byte)11),
-    MM_IDCARD_3M("MM_IDCARD_3M","identityAssociatedPhone3MCntCopy", (byte)12);
-
+    MP_LOAN_12M("MP_LOAN_12M", "loanApply12MCntCopy", (byte)1),
+    MP_LOAN_6M("MP_LOAN_6M", "loanApply6MCntCopy", (byte)2),
+    MP_LOAN_3M("MP_LOAN_3M", "loanApply3MCntCopy", (byte)3),
+    MP_LOAN_1M("MP_LOAN_1M", "loanApply1MCntCopy", (byte)4),
+    MD_LOAN_1W("MD_LOAN_1W", "loanApply7DCntCopy", (byte)5),
+    MD_IDCARD_1M("MD_IDCARD_1M", "identityAssociatedDevice1MCntCopy", (byte)6),
+    MD_IDCARD_1W("MD_IDCARD_1W", "deviceAssociatedIdentity7DCntCopy", (byte)7),
+    MM_DEVICE_1W("MM_DEVICE_1W", "deviceAssociatedPhone7DCntCopy", (byte)8),
+    MI_DEVICE_1W("MI_DEVICE_1W", "identityAssociatedDevice7DCntCopy", (byte)9),
+    MI_MOBILE_3W("MI_MOBILE_3W", "phoneAssociatedIdentity3MCntCopy", (byte)10),
+    ME_IDCARD_3M("ME_IDCARD_3M", "identityAssociatedMail3MCntCopy", (byte)11),
+    MM_IDCARD_3M("MM_IDCARD_3M", "identityAssociatedPhone3MCntCopy", (byte)12);
 
     private String name;
     private String text;
     private Byte code;
 
-    ETongdunData(String name,String text, Byte code) {
-        this.name= name;
+    ETongdunData(String name, String text, Byte code) {
+        this.name = name;
         this.text = text;
         this.code = code;
     }
@@ -61,6 +64,7 @@ public enum ETongdunData {
         }
         return -1;
     }
+
     public static String getText(Byte code) {
         if (Objects.nonNull(code)) {
             for (ETongdunData item : ETongdunData.values()) {
