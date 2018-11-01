b3f644be04c0e4ce64edbf8579c07172a82eec41
guoguoyun <guoguoyun@treefinance.com.cn>
2018-11-01 14:22:24 +0800

    saas服务接口返回改造

--- common/src/main/java/com/treefinance/saas/grapserver/common/enums/ECode.java
+++ common/src/main/java/com/treefinance/saas/grapserver/common/enums/ECode.java
@@ -6,30 +6,36 @@ import java.util.Objects;
  * @author:guoguoyun
  * @date:Created in 2018/10/30下午2:48
  */
+
 public enum ECode {
-    UNAUTHORIZED_APPIDINVALID(1,"商户未授权","AppIdInvalidException","商户未授权,isvalid=false"),
-    UNAUTHORIZED_APPIDFORMAT(2,"商户ID不符合格式","AppIdFormatException","appId不符合格式"),
-    FORBIDDEN_APPIDNOTACTIVE(3,"商户被禁用","AppIdNotActiveException","appID被禁用，isActive=false"),
-    FORBIDDEN_APPIDNOMESSAGE(4,"商户无可用数据","AppIdNoMessageException","appID无信息"),
-    FORBIDDEN_UNIQUEIDMAX(5,"商户访问超过阈值","UniqueidMaxException","商户访问超过阈值"),
-    VALIDATION(6,"传入参数不合法","ValidationException","appid,mobile,idcard,email格式不合法"),
-    CRPTOR_REQUESTDECRYPT(7,"解密请求参数异常","RequestDecryptException","解密请求参数"),
-    CRPTOR(8,"加解密方法出现异常","CryptorException","加解密方法出现异常求参数"),
-    CRPTOR_RESPONSEENCRYPT(9,"加密返回参数异常","ResponseEncryptException","解密请求参数"),
-    BIZ(10,"业务异常","BizException","暂定"),
-    PARAMSCHECK(11,"参数异常","ParamsCheckException","具体含义自定义"),
-    RESPONSE(12,"服务器内部异常","ResponseException","返回数据异常"),
-    UNKNOW(201,"服务器内部异常","UnknownException","调用taskcenter异常"),
-    TASKTIMEOUT(202,"服务器内部异常","TaskTimeOutException","任务超时异常"),
-    REQUESTFAILED(203,"服务器内部异常","RequestFailedException","请求失败"),
-    NOTFOUND(204,"服务器内部异常","NotFoundException","转发的URL没有对应服务"),
-    TIMEOUT(205,"服务器内部异常","TimeOutException","上游服务器没有响应，超时");
 
+    EXCEPTION(-1, "服务器内部异常", "EXCEPTION", "具体含义自定义"),
 
+    UNAUTHORIZED_APPIDINVALID(1, "商户未授权", "AppIdInvalidException", "商户未授权,isvalid=false"),
+    UNAUTHORIZED_APPIDFORMAT(2, "商户ID不符合格式", "AppIdFormatException", "appId不符合格式"),
+    FORBIDDEN_APPIDNOTACTIVE(3, "商户被禁用", "AppIdNotActiveException", "appID被禁用，isActive=false"),
+    FORBIDDEN_APPIDNOMESSAGE(4, "商户无可用数据", "AppIdNoMessageException", "appID无信息"),
+    FORBIDDEN_UNIQUEIDMAX(5, "商户访问超过阈值", "UniqueidMaxException", "商户访问超过阈值"),
+    VALIDATION(6, "传入参数不合法", "ValidationException", "具体含义自定义"),
+    CRPTOR_REQUESTDECRYPT(7, "解密请求参数异常", "RequestDecryptException", "解密请求参数"),
+    CRPTOR(8, "加解密方法出现异常", "CryptorException", "加解密方法出现异常求参数"),
+    CRPTOR_RESPONSEENCRYPT(9, "加密返回参数异常", "ResponseEncryptException", "解密请求参数"),
+    BIZ(10, "业务异常", "BizException", "自定义"),
+    PARAMSCHECK(11, "参数异常", "ParamsCheckException", "具体含义自定义"),
+    RESPONSE(12, "服务器内部异常", "ResponseException", "返回数据异常"),
+    UNKNOW(201, "服务器内部异常", "UnknownException", "调用taskcenter异常"),
+    TASKTIMEOUT(202, "服务器内部异常", "TaskTimeOutException", "任务超时异常"),
+    REQUESTFAILED(203, "服务器内部异常", "RequestFailedException", "请求失败"),
+    NOTFOUND(204, "服务器内部异常", "NotFoundException", "转发的URL没有对应服务"),
+    TIMEOUT(205, "服务器内部异常", "TimeOutException", "上游服务器没有响应，超时");
 
+    /**自定义code码*/
     private int code;
+    /**返回给第三方的错误信息*/
     private String msg;
+    /**对应的异常类*/
     private String exception;
+    /**系统内部报错日志信息，某些异常可自定义错误内容*/
     private String description;
 
     ECode(int code, String msg, String exception, String description) {
@@ -71,7 +77,6 @@ public enum ECode {
         this.description = description;
     }
 
-
     public static int getCode(String exception) {
         if (Objects.nonNull(exception)) {
             for (ECode item : ECode.values()) {
@@ -82,8 +87,6 @@ public enum ECode {
         }
         return -1;
     }
-
-
 
     public static ECode from(String exception) {
         try {
@@ -102,6 +105,13 @@ public enum ECode {
         return null;
     }
 
-
+    public static String getDescription(int code) {
+        for (ECode item : ECode.values()) {
+            if (item.code == code) {
+                return item.getDescription();
+            }
+        }
+        return null;
+    }
 
 }
--- web/src/main/java/com/treefinance/saas/grapserver/web/advice/SaasExceptionAdvice.java
+++ web/src/main/java/com/treefinance/saas/grapserver/web/advice/SaasExceptionAdvice.java
@@ -2,6 +2,7 @@ package com.treefinance.saas.grapserver.web.advice;
 
 import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Maps;
+import com.treefinance.saas.grapserver.common.enums.ECode;
 import com.treefinance.saas.grapserver.common.exception.*;
 import com.treefinance.saas.grapserver.common.exception.base.MarkBaseException;
 import com.treefinance.saas.grapserver.common.result.SaasResult;
@@ -65,6 +66,7 @@ public class SaasExceptionAdvice extends ResponseEntityExceptionHandler {
         handleLog(request, ex);
         return handleExceptionInternal(ex, buildBody(ex), null, HttpStatus.UNAUTHORIZED, request);
     }
+
     @ExceptionHandler(value = UniqueidMaxException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
     @ResponseBody
@@ -88,6 +90,14 @@ public class SaasExceptionAdvice extends ResponseEntityExceptionHandler {
         return handleExceptionInternal(ex, buildBody(ex), null, HttpStatus.FORBIDDEN, request);
     }
 
+    @ExceptionHandler(value = ValidationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ResponseEntity<Object> handleValidationException(WebRequest request, ValidationException ex) {
+        handleLog(request, ex);
+        return handleExceptionInternal(ex, buildBody(ex), null, HttpStatus.BAD_REQUEST, request);
+    }
+
     @ExceptionHandler(Exception.class)
     @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
     @ResponseBody
@@ -104,20 +114,6 @@ public class SaasExceptionAdvice extends ResponseEntityExceptionHandler {
         return handleExceptionInternal(ex, buildBody(ex), null, HttpStatus.BAD_REQUEST, request);
     }
 
-    @ExceptionHandler(ValidationException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ResponseBody
-    public ResponseEntity<Object> handleValidationException(ValidationException ex, WebRequest request) {
-        handleLog(request, ex);
-        return handleExceptionInternal(ex, buildBody(ex), null, HttpStatus.BAD_REQUEST, request);
-    }
-
-    @Override
-    public ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex,
-        HttpHeaders headers, HttpStatus status, WebRequest request) {
-        return handleExceptionInternal(ex, buildBody(ex), headers, status, request);
-    }
-
     private void handleLog(WebRequest request, Exception ex) {
         StringBuffer logBuffer = new StringBuffer();
         if (request != null) {
@@ -131,29 +127,69 @@ public class SaasExceptionAdvice extends ResponseEntityExceptionHandler {
 
     private Object buildBody(Exception ex) {
         logger.error("系统异常", ex);
-       SaasResult result = new SaasResult();
+        SaasResult result = new SaasResult();
 
-        if (ex instanceof ForbiddenException) {
-            result.setCode(-1);
-            result.setMsg("用户未授权");
-        } else if (ex instanceof UnAuthorizedException) {
-            result.setCode(1);
-            result.setMsg(ex.getMessage());
-        } else if (ex instanceof MarkBaseException) {
-            result.setCode(2);
-            result.setMsg("服务器内部异常");
-        } else if (ex instanceof MissingServletRequestParameterException) {
-            result.setMsg("参数异常");
+        if (ex instanceof AppIdInvalidException) {
+            result.setCode(ECode.UNAUTHORIZED_APPIDINVALID.getCode());
+            result.setMsg(ECode.UNAUTHORIZED_APPIDINVALID.getMsg());
+        } else if (ex instanceof AppIdFormatException) {
+            result.setCode(ECode.UNAUTHORIZED_APPIDFORMAT.getCode());
+            result.setMsg(ECode.UNAUTHORIZED_APPIDFORMAT.getMsg());
+        } else if (ex instanceof AppIdNotActiveException) {
+            result.setCode(ECode.FORBIDDEN_APPIDNOTACTIVE.getCode());
+            result.setMsg(ECode.FORBIDDEN_APPIDNOTACTIVE.getMsg());
         } else if (ex instanceof BizException) {
-            result.setMsg(ex.getMessage());
-            return result;
+            result.setCode(ECode.BIZ.getCode());
+            result.setMsg(ECode.BIZ.getMsg());
         } else if (ex instanceof ParamsCheckException) {
-            result.setMsg(ex.getMessage());
+            result.setCode(ECode.PARAMSCHECK.getCode());
+            result.setMsg(ECode.PARAMSCHECK.getMsg());
+            logger.error("exception handle : ex={},description={} result={}", ex.getClass(), ex.getMessage(),
+                JSON.toJSONString(result));
+            return result;
+        } else if (ex instanceof ValidationException) {
+            result.setCode(ECode.VALIDATION.getCode());
+            result.setMsg(ECode.VALIDATION.getMsg());
+            logger.error("exception handle : ex={},description={} result={}", ex.getClass(), ex.getMessage(),
+                JSON.toJSONString(result));
+            return result;
+        } else if (ex instanceof ResponseException) {
+            result.setCode(ECode.RESPONSE.getCode());
+            result.setMsg(ECode.RESPONSE.getMsg());
+        } else if (ex instanceof UnknownException) {
+            result.setCode(ECode.UNKNOW.getCode());
+            result.setMsg(ECode.UNKNOW.getMsg());
+        } else if (ex instanceof RequestFailedException) {
+            result.setCode(ECode.REQUESTFAILED.getCode());
+            result.setMsg(ECode.REQUESTFAILED.getMsg());
+        } else if (ex instanceof TaskTimeOutException) {
+            result.setCode(ECode.TASKTIMEOUT.getCode());
+            result.setMsg(ECode.TASKTIMEOUT.getMsg());
+        } else if (ex instanceof CryptorException) {
+            result.setCode(ECode.CRPTOR.getCode());
+            result.setMsg(ECode.CRPTOR.getMsg());
+        } else if (ex instanceof RequestDecryptException) {
+            result.setCode(ECode.CRPTOR_REQUESTDECRYPT.getCode());
+            result.setMsg(ECode.CRPTOR_REQUESTDECRYPT.getMsg());
+        } else if (ex instanceof ResponseEncryptException) {
+            result.setCode(ECode.CRPTOR_RESPONSEENCRYPT.getCode());
+            result.setCode(ECode.CRPTOR_RESPONSEENCRYPT.getCode());
+        } else if (ex instanceof TaskTimeOutException) {
+            result.setCode(ECode.TASKTIMEOUT.getCode());
+            result.setCode(ECode.TASKTIMEOUT.getCode());
+        } else if (ex instanceof Exception) {
+            result.setCode(ECode.EXCEPTION.getCode());
+            result.setMsg(ECode.EXCEPTION.getMsg());
+            logger.error("exception handle : ex={},description={} result={}", ex.getClass(), ex.getMessage(),
+                JSON.toJSONString(result));
+            return result;
         }
         if (StringUtils.isEmpty(result.getMsg())) {
-            result.setMsg("系统忙，请稍后重试");//暂时
+            result.setCode(-1);
+            result.setMsg("系统忙，请稍后重试");
         }
-        logger.info("exception handle : ex={},errormessage={} result={}", ex.getClass(), ex.getMessage(),JSON.toJSONString(result));
+        logger.error("exception handle : ex={},description={} result={}", ex.getClass(),
+            ECode.getDescription(result.getCode()), JSON.toJSONString(result));
         return result;
     }
 
