0d2e135b11d0405c1fadbbbe43f36cf0e4a972c7
桂美超 <guimeichao@treefinance.com.cn>
2019-01-07 18:37:04 +0800

    在grap-server拉取企业列表并发给爬数爬取

--- biz/src/main/java/com/treefinance/saas/grapserver/biz/service/EnterpriseInformationService.java
+++ biz/src/main/java/com/treefinance/saas/grapserver/biz/service/EnterpriseInformationService.java
@@ -1,7 +1,11 @@
 package com.treefinance.saas.grapserver.biz.service;
 
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import com.datatrees.spider.extra.api.EnterpriseApi;
 import com.google.gson.reflect.TypeToken;
 import com.treefinance.saas.grapserver.biz.config.DiamondConfig;
 import com.treefinance.saas.grapserver.common.enums.EBizType;
@@ -33,6 +37,9 @@ public class EnterpriseInformationService {
     @Autowired
     private AcquisitionService acquisitionService;
 
+    @Resource
+    private EnterpriseApi      enterpriseApi;
+
     private static final Logger logger = LoggerFactory.getLogger(EnterpriseInformationService.class);
 
     public Long creatTask(String appId, String uniqueId) {
@@ -46,6 +53,18 @@ public class EnterpriseInformationService {
         if (StringUtils.isBlank(website)) {
             return SaasResult.failResult("当前平台不支持!");
         }
+        Map param = GsonUtils.fromJson(extra, new TypeToken<Map>() {}.getType());
+        String keyword = (String) param.get("business");
+        List<Map<String, String>> enterpriseList = enterpriseApi.queryEnterprise(keyword, website);
+        StringBuilder extraValue = new StringBuilder();
+        enterpriseList.stream().forEach(enterprise -> extraValue.append(enterprise.get("name")).append(":").append(enterprise.get("unique")).append(":")
+                .append(enterprise.get("index")).append(";"));
+        if (StringUtils.isBlank(extraValue)) {
+            return SaasResult.failResult("企业列表为空");
+        }
+        Map<String, String> extraMap = new HashMap<>();
+        extraMap.put("business", extraValue.toString());
+        extra = GsonUtils.toJson(extraMap);
         logger.info("工商信息-发消息：acquisition，taskid={},extra={}", taskid, extra);
         acquisitionService.acquisition(taskid, null, null, null, website, null, ESpiderTopic.SPIDER_EXTRA.name().toLowerCase(), extra);
         return SaasResult.successResult(taskid);
