ff0537de40fbd0c0558c8e976e2f5646f1dc26b0
zhangyanjia <zhangyanjia@treefinance.com.cn>
2019-01-22 16:43:01 +0800

    增加http接口

---
+++ biz/src/main/java/com/treefinance/saas/grapserver/biz/service/TaskPointService.java
@@ -0,0 +1,30 @@
+package com.treefinance.saas.grapserver.biz.service;
+
+import com.alibaba.fastjson.JSON;
+import com.treefinance.saas.taskcenter.facade.request.TaskPointRequest;
+import com.treefinance.saas.taskcenter.facade.result.common.TaskResult;
+import com.treefinance.saas.taskcenter.facade.service.TaskPointFacade;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * @author 张琰佳
+ * @since 4:34 PM 2019/1/22
+ */
+@Service
+public class TaskPointService {
+    private static final Logger logger = LoggerFactory.getLogger(TaskPointService.class);
+
+    @Resource
+    private TaskPointFacade taskPointFacade;
+
+    public void addTaskPoint(TaskPointRequest taskPointRequest){
+        TaskResult<Void> result=taskPointFacade.addTaskPoint(taskPointRequest);
+        if (!result.isSuccess()){
+            logger.error("埋点记录失败,taskPointRequest={}", JSON.toJSONString(taskPointRequest));
+        }
+    }
+}
---
+++ web/src/main/java/com/treefinance/saas/grapserver/web/controller/TaskPointController.java
@@ -0,0 +1,25 @@
+package com.treefinance.saas.grapserver.web.controller;
+
+import com.treefinance.saas.grapserver.biz.service.TaskPointService;
+import com.treefinance.saas.taskcenter.facade.request.TaskPointRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 张琰佳
+ * @since 4:31 PM 2019/1/22
+ */
+@RestController
+@RequestMapping(value = "/taskPoint")
+public class TaskPointController {
+    @Autowired
+    private TaskPointService taskPointService;
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public void addTaskPoint(@RequestBody TaskPointRequest taskPointRequest) {
+        taskPointService.addTaskPoint(taskPointRequest);
+    }
+}
