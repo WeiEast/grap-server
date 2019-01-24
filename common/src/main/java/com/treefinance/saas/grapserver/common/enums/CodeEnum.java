b56af769a7e580e1463a12bd1aec9611de84ebf0
henengqiang <henengqiang@treefintech.com>
2019-01-24 17:30:39 +0800

    埋点枚举

--- common/src/main/java/com/treefinance/saas/grapserver/common/enums/CodeEnum.java
+++ common/src/main/java/com/treefinance/saas/grapserver/common/enums/CodeEnum.java
@@ -39,16 +39,35 @@ public enum CodeEnum {
     ECOMMERCE_AUTHORIZATION_("30100302","100803"),
     ECOMMERCE_AUTHORIZATION_USERAGREEMENT("30100303","100802"),
     ECOMMERCE_AUTHORIZATION_FAQ("30100305","100804"),
-    ECOMMERCE_AUTHORIZATION_FEEDBACK("30100306","100805");
+    ECOMMERCE_AUTHORIZATION_FEEDBACK("30100306","100805"),
 
-
-
-
-
-
-
-
-
+    /*
+     * 邮箱账单
+     */
+    /** 邮箱账单流程启动，加载首页 */
+    EMAIL_START("20100101", "200001"),
+    /** 选择QQ邮箱。点击"QQ邮箱” */
+    EMAIL_CHOOSE_QQ("20100102", "200002"),
+    /** 选择163邮箱。点击“163邮箱” */
+    EMAIL_CHOOSE_163("20100103", "200003"),
+    /** 选择126邮箱。点击“126邮箱” */
+    EMAIL_CHOOSE_126("20100104", "200004"),
+    /** 选择新浪邮箱。点击“新浪邮箱” */
+    EMAIL_CHOOSE_SINA("20100105", "200005"),
+    /** 选择139邮箱。点击“139邮箱” */
+    EMAIL_CHOOSE_139("20100106", "200006"),
+    /** 选择企业邮箱。点击“企业邮箱” */
+    EMAIL_CHOOSE_QQEXMAIL("20100107", "200110"),
+    /** 勾选个人信息查询授权书 */
+    EMAIL_SELECT_AUTHORIZATION("20100108", "200107"),
+    /** 点击查看个人信息查询授权书页面 */
+    EMAIL_LOOK_AUTHORIZATION("20100110", "200109"),
+    /** 退出个人信息查询授权书页面 */
+    EMAIL_EXIT_AUTHORIZATION_PAGE("20100111", "200301"),
+    /** 邮箱列表页，点击返回 */
+    EMAIL_BACK("20100112", "200401"),
+    /** 第三方登录页面，点击返回 */
+    EMAIL_THIRD_PAGE_BACK("20100201", "200401");
 
 
     private String key;
